7276d08c358f7e9c06968eab031721b33ed19f8b
Patrick Hrabos <phrabos@gmail.com>
2020-12-03 19:35:06 -0500

    adds buttons to change color of name tag

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const button = document.getElementById('button');
 const input = document.getElementById('input');
 const name = document.getElementById('name');
+const purpleBtn = document.getElementById('purple');
+const redBtn = document.getElementById('red');
+const pinkBtn = document.getElementById('pink');
+const nameTag = document.getElementById('main');
 
 button.addEventListener('click', () => {  
 
     name.innerText = input.value;
     input.value = "";
+    
+})
+
+purpleBtn.addEventListener('click', () => {  
+
+    nameTag.style.backgroundColor = "purple";
+    
+})
+
+redBtn.addEventListener('click', () => {  
+
+    nameTag.style.backgroundColor = "red";
+    
+})
+
+pinkBtn.addEventListener('click', () => {  
+
+    nameTag.style.backgroundColor = "magenta";
     
 })
